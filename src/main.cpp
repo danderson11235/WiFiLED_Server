a546830def8ee917eef4148d6574028e5dd34784
danderson11235 <danderson11235@gmail.com>
2021-05-17 15:45:23 -0400

    hmm well that changes a lot

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <winsock2.h>
 #include <endpointvolume.h>
-#include <time.h>
-#include "../include/kiss_fft130/kiss_fft.h"
+#include <ctime>
+#include "../include/kissfft-master/kiss_fftr.h"
+#include "../include//kissfft-master/_kiss_fft_guts.h"
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
 
@@ -11,15 +12,22 @@
 #define BUFLEN 512	//Max length of buffer
 #define PORT 4210	//The port on which to listen for incoming data
 
+const int nfft = 1024;
+const float freq = 31250;
+const float peri = 1 / freq;
+const float c_peri = 1000000 * peri;
 void createMessage();
 
 int main(void)
 {
 	struct sockaddr_in si_other;
 	int s, slen = sizeof(si_other);
-	char buf[BUFLEN];
 	char message[BUFLEN] = "send";
 	WSADATA wsa;
+
+	kiss_fftr_cfg cfg = kiss_fftr_alloc(nfft, 0, 0, 0);
+	float in[nfft];
+	kiss_fft_cpx out[nfft];
 
 	//Initialise winsock
 	printf("\nInitialising Winsock...");
@@ -44,10 +52,23 @@ int main(void)
 	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 
 	//start communication
+	clock_t c_count_old = clock();
+	clock_t c_count_new = clock();
 
 	while (true)
 	{
-		
+		if (c_count_new - c_count_old >= c_peri)
+		{
+			c_count_old = c_count_new;
+			for (int i = nfft - 1; i > 1; i--)
+			{
+				in[i] = in[i - 1];
+			}
+			float newVol;
+			//IAudioEndpointVolume::GetMasterVolumeLevelScalar(&newVol);
+			kiss_fftr(cfg, in, out);
+		}
+		c_count_new = clock();
 	}
 	//send the message
 	if (sendto(s, message, strlen(message), 0, (struct sockaddr*)&si_other, slen) == SOCKET_ERROR)
@@ -59,6 +80,7 @@ int main(void)
 	closesocket(s);
 	WSACleanup();
 
+	kiss_fftr_free(cfg);
 	return 0;
 }
 
